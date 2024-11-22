4baad966dffce710df6e4a4d4f8efb8c000e3ccc
Ayoub-Elazhari <a.elazhari0495@uca.ac.ma>
2024-11-22 16:50:13 +0100

    printf

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aelelz <aelelz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 13:52:01 by aelelz            #+#    #+#             */
-/*   Updated: 2024/11/19 18:28:00 by aelelz           ###   ########.fr       */
+/*   Updated: 2024/11/22 16:49:34 by aelelz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,14 @@
 
 # include <stdarg.h>
 # include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
 
 int		ft_printf(const char *format, ...);
-int		ft_print_point(unsigned long long num);
-int		ft_point_count(unsigned long long num);
+int		ft_checker(const char str, va_list args);
 void	ft_point(unsigned long long num);
-void	ft_check(const char *str, va_list args);
+int		ft_point_count(unsigned long long num);
+int		ft_print_point(unsigned long long num);
 int		ft_putchar(int c);
 int		ft_put_nbr(int nb);
-int		ft_print_str(char *s);
 int		ft_putstr(char *s);
 int		ft_print_unsigned(unsigned int num);
 int		ft_print_hex(unsigned int num, const char format);
